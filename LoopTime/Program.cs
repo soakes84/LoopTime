92c47d8ac572f87a162f34f311b42a670983d322
soakes84 <spencer.oakes84@gmail.com>
2019-05-09 22:03:31 -0400

    checking times of different for loops

--- LoopTime/Program.cs
+++ LoopTime/Program.cs
@@ -10,12 +10,27 @@ namespace LoopTime
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            MyStopwatch sw = new MyStopwatch();
+            sw.Start();
+            for (int i = 0; i < 5000; i++) Console.WriteLine(i);
+            sw.Stop();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            MyStopwatch sw1 = new MyStopwatch();
+            int[] array = Enumerable.Range(0, 5000).ToArray();
+            sw1.Start();
+            for (int i = 0; i < array.Length; i++) Console.WriteLine(array[i]);
+            sw1.Stop();
+
+            MyStopwatch sw2 = new MyStopwatch();
+            var arrList = array.ToList();
+            sw2.Start();
+            for (int i = 0; i < arrList.Count; i++) Console.WriteLine(arrList[i]);
+            sw2.Stop();
+
+            Console.WriteLine($"for loop- time elapsed: {sw.ElapsedMilliseconds} Milliseconds, StartAt: {sw.StartAt.Value}, EndAt: {sw.EndAt.Value}");
+            Console.WriteLine($"for loop on array- time elapsed: {sw1.ElapsedMilliseconds} Milliseconds, StartAt: {sw1.StartAt.Value}, EndAt: {sw1.EndAt.Value}");
+            Console.WriteLine($"for loop on list- time elapsed: {sw2.ElapsedMilliseconds} Milliseconds, StartAt: {sw2.StartAt.Value}, EndAt: {sw2.EndAt.Value}");
+            Console.ReadKey();
         }
     }
 }
