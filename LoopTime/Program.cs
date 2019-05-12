61c8735810527ae53540719b9b938f2840713a42
soakes84 <spencer.oakes84@gmail.com>
2019-05-12 19:42:31 -0400

    while loop

--- LoopTime/Program.cs
+++ LoopTime/Program.cs
@@ -39,11 +39,26 @@ namespace LoopTime
             foreach (var arr in arrList) Console.WriteLine(arr);
             sw4.Stop();
 
+            MyStopwatch sw5 = new MyStopwatch();
+            sw5.Start();
+            int j = 0;
+            while (j != array.Length) Console.WriteLine(array[j++]);
+            sw5.Stop();
+
+            MyStopwatch sw6 = new MyStopwatch();
+            sw6.Start();
+            j = 0;
+            while (j != arrList.Count) Console.WriteLine(arrList[j++]);
+            sw6.Stop();
+
             Console.WriteLine($"for loop- time elapsed: {sw.ElapsedMilliseconds} Milliseconds, StartAt: {sw.StartAt.Value}, EndAt: {sw.EndAt.Value}");
             Console.WriteLine($"for loop on array- time elapsed: {sw1.ElapsedMilliseconds} Milliseconds, StartAt: {sw1.StartAt.Value}, EndAt: {sw1.EndAt.Value}");
             Console.WriteLine($"for loop on list- time elapsed: {sw2.ElapsedMilliseconds} Milliseconds, StartAt: {sw2.StartAt.Value}, EndAt: {sw2.EndAt.Value}");
             Console.WriteLine($"foreach on array- time elapsed: {sw3.ElapsedMilliseconds} Milliseconds, StartAt: {sw3.StartAt.Value}, EndAt: {sw3.EndAt.Value}");
             Console.WriteLine($"foreach on list- time elapsed: {sw4.ElapsedMilliseconds} Milliseconds, StartAt: {sw4.StartAt.Value}, EndAt: {sw4.EndAt.Value}");
+            Console.WriteLine($"while loop on array- time elapsed: {sw5.ElapsedMilliseconds} Milliseconds, StartAt: {sw5.StartAt.Value}, EndAt: {sw5.EndAt.Value}");
+            Console.WriteLine($"while loop on list- time elapsed: {sw6.ElapsedMilliseconds} Milliseconds, StartAt: {sw6.StartAt.Value}, EndAt: {sw6.EndAt.Value}");
+
             Console.ReadKey();
 
 
